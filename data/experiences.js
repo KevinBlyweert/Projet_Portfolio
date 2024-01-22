3899b37bda61853273edeb577220ffa194cc677a
Neithan et Eldalote Coemgen <kevin.blyweert@gmail.com>
2024-01-22 15:00:55 +0100

    new look for portfolio

---
+++ data/experiences.js
@@ -0,0 +1,30 @@
+const experiences = [
+    {
+        "start":"2012",
+        "end":"2012",
+        "label":"Stage de fin d'année",
+        "location":"Bill Mikro - Aix-Noulette(62)",
+        "detail":"Mise à jour du site internet via Joomla!, inventaire de la boutique et entrée des données dans SAP, accueil des clients, réparations diverses et montage de PC."
+    },
+    {
+        "start":"2012",
+        "end":"2013",
+        "label":"Développeur d'applications",
+        "location":"Satcoms - Roeux(62)",
+        "detail":"Création d'applications pour la gestion interne de la société en utilisant le logiciel WinDev."
+    },
+    {
+        "start":"2013",
+        "end":"2019",
+        "label":"Réceptionnaire en logistique",
+        "location":"ID Logistics - Brebières et Dourges (62)",
+        "detail":"Contrôle quantitatif et qualitatif des marchandises en entrée, déchargement des camions, stockage des palettes (CACES 1,3 et 5). Accueil des chauffeurs, remplissage des tableaux IntraNet. Remplacement du chef d'équipe."
+    },
+    {
+        "start":"2019",
+        "end":"2023",
+        "label":"Agent administratif d'exploitation",
+        "location":"ID Logistics - Dourges (62)",
+        "detail":"Point d'entrée de toute demande sur l'activité par les 350 fournisseurs par mail et téléphone, création de tableurs et requêtes Excel pour répondre à toute demande, envoi des traçabilités, création / mise à jour des produits, gestion des contrats-date, devis, organisation de transport..."
+    }
+]
