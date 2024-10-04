ae74895bc906b37cb5f2faf3a03601dad20c7b58
Neithan et Eldalote Coemgen <kevin.blyweert@gmail.com>
2024-10-04 23:09:02 +0200

    working on the new layout, side panels still to work on

--- styles/To_come/index.js
+++ styles/To_come/index.js
@@ -1,21 +1,21 @@
-import { experiences } from "../data/experiences.js";
-import { scolarship } from "../data/scolarship.js";
-import { skills } from "../data/skills.js";
-import { achievements } from "../data/achievements.js";
-import { RPS } from './rock-paper-scisors.js'
-import { createPlayground } from "./color_tiles.js";
-import { reactModal } from "./reactModal.js";
-import { vueModal } from "./vueModal.js";
-import utilities from "./utilities.js";
+// import { experiences } from "../data/experiences.js";
+// import { scolarship } from "../data/scolarship.js";
+// import { skills } from "../data/skills.js";
+// import { achievements } from "../data/achievements.js";
+// import { RPS } from './rock-paper-scisors.js'
+// import { createPlayground } from "./color_tiles.js";
+// import { reactModal } from "./reactModal.js";
+// import { vueModal } from "./vueModal.js";
+// import utilities from "./utilities.js";
 
 const app = {
-
-    toggleTheme: (event) => {
-        document.body.classList.toggle("light");
-        document.body.classList.toggle("dark");
+    openPanel: (e) => {
+        const panel = document.querySelector(`.panel.${e.target.dataset.id}`)
+        panel.classList.add("active");
+        panel.querySelector(".close").addEventListener("click", () => panel.classList.remove("active"))
     },
     init: () => {
-        document.querySelector(".themeToggle").addEventListener("click", app.toggleTheme);
+        document.querySelectorAll(".grid button").forEach(button => button.addEventListener("click", app.openPanel));
     }
 }
 
